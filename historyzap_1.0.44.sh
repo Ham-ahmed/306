aaafa403b8e74142a6836f19b16ae2a09a3e9261
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-06-30 00:52:05 +0300

    Add files via upload

---
+++ historyzap_1.0.44.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-historyzap_1.0.44_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/306/refs/heads/main/enigma2-plugin-extensions-historyzap_1.0.44_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-historyzap_1.0.44_all.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "#########################################################"
+echo "#                INSTALLED SUCCESSFULLY                 #"
+echo "#                    ON - Panel v6                      #"
+echo "#             Enigma2 restart is required               #"
+echo "#        .::UPLOADED BY  >>>>   HAMDY_AHMED::.          #"
+echo "#     https://www.facebook.com/share/g/18qCRuHz26/      #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+wait
+killall -9 enigma2
+exit 0
